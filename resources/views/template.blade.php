693afc1bff3d58da59f22ba7d4e6aa99f98f6e02
tolotravalery <valeryharisoanitolotra@gmail.com>
2017-08-31 10:08:06 +0200

    home page

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -32,7 +32,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="{{url('/')}}">
                 <img src="{{ url('/') }}/img/logo.png" title="tapakila">
             </a>
         </div>
