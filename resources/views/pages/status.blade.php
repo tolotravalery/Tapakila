43aea1c0d0df94a6f1cbec0209a565c92f9681bc
koera-sam <tolotrarazafindrabe@gmail.com>
2017-09-08 13:11:49 +0200

    Udapte dev edit profile

--- resources/views/pages/status.blade.php
+++ resources/views/pages/status.blade.php
@@ -1,6 +1,5 @@
 @extends("template")
 @section('content')
-
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -8,5 +7,4 @@
             </div>
         </div>
     </div>
-
 @endsection
