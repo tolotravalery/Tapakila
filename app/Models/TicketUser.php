c8ee7997fd18c442c72077fe1cb6aa3467eb8668
tolotravalery <valeryharisoanitolotra@gmail.com>
2017-12-19 11:56:36 +0100

    annuler payment

---
+++ app/Models/TicketUser.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Trustylabs
+ * Date: 19/12/2017
+ * Time: 11:45
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class TicketUser extends Model
+{
+    protected $table = 'ticket_user';
+}
