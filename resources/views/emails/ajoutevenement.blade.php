e6481715c65336f22e268fb32ac813dc54b04403
Andrysitraka <andrewsitraka@gmail.com>
2018-02-12 16:59:21 +0300

    email page modif et page evenement

--- resources/views/emails/ajoutevenement.blade.php
+++ resources/views/emails/ajoutevenement.blade.php
@@ -1,79 +1,73 @@
-<style>
-    @media only screen and (max-width: 600px) {
-        .inner-body {
-            width: 100% !important;
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <style>
+        @media only screen and (max-width: 600px) {
+            .inner-body {
+                width: 100% !important;
+            }
+
+            .footer {
+                width: 100% !important;
+            }
         }
 
-        .footer {
-            width: 100% !important;
+        #globcontent {
+            background-color: white;
+            margin-top: 84px;
+            padding: 20px 30px 20px 30px;
+            overflow: hidden;
+            margin-bottom: 83px;
+            border-radius: 3px;
         }
-    }
 
-</style>
-<body style="background-color:#eeeeee;">
-<table class="wrapper" width="100%" cellpadding="0" cellspacing="0">
-    <tr>
-        <td style="background-color: #d70506;padding: 2px">
-            <img src="{{url('/')}}/public/img/logo.png" style="padding-left: -5px;">
-        </td>
-    </tr>
-</table>
-<table class="wrapper" width="100%" style="margin-top:-20px;margin-left:18px; ">
-    <tr>
-        <td>
-            <h3 style="font-size: 30px; font-family:sans-serif;color:#333;"><b></b></h3>
-            <p style="font-family:sans-serif;font-size: 18px;color: #d70506;padding-top: -34px;margin: 0 0 10px;"></p>
-        </td>
-    </tr>
-</table>
+        .mim {
+            border-bottom: 1px solid black;
+            padding-bottom: 2px;
+        }
+    </style>
 
-<table class="wrapper" width="100%" cellpadding="0" cellspacing="0" style="margin-top:-20px">
-    <tr>
-        <td align="center">
-            <h2>
-                <br/><b style=" color:#333;margin-top: 35px; margin-bottom: 10px;font-size: 30px;word-wrap: break-word;font-weight: 700;font-family:Lucida Console;">Bonjour {{$user->name}},</b>
-            </h2>
-            <p style="font-size: 14px;color:#333;">Votre événement a été bien ajouté, nous vous remerçions de votre confiance.</p><br>
-
-        </td>
-    </tr>
-	<tr>
-		<td align="center">
-			<table>
-				<tr>
-					<td>
-					<div align="left">
-				<p style="font-size: 14px;color:#333;"><b>Titre: </b>{{$event->title}} </p>
-				<p style="font-size: 14px;color:#333;"><b>Description
-				: </b>{{$event->additional_note}} </p>
-				<p style="font-size: 14px;color:#333;"><b>Programme
-				: </b>{{$event->additional_note_time}} </p>
-				<p style="font-size: 14px;color:#333;"><b>Localisation
-				: </b>{{$event->localisation_nom}} {{$event->localisation_adresse}}
-				</p>
-				<p style="font-size: 14px;color:#333;"><b>Date
-				: </b>le {{$event->date_debut_envent->format('d M Y H:i:s')}}
-				à {{$event->date_fin_event->format('d M Y H:i:s')}} </p><br>
-			</div>
-					</td>
-				</tr>
-			</table>
-		</td>
-	</tr>
-</table>
-<br/>
-<div class="container">
-    <div class="row ">
-
+</head>
+<body>
+<div id="cp">
+    <div id="header" style="width:100%;height:100px;background:#d70506;color:white;">
+        <img src="{{url('/')}}/public/img/logo.png" style="padding-left: -5px;">
+        <div id="textheader" style="position:relative; width:400px; margin:auto;  font-size:18px; padding-top:40px;text-align: center; text-decoration:none;color:white;"><a style="text-decoration:none;color:white;" href="https://leguichet.mg/faq">www.leguichet.mg</a></div>
     </div>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-offset-3 col-lg-6">
-            <div style=" color: white;text-align: center;background-color: #d70506;font-size: 16px;padding: 1px;font-family:sans-serif;">
-                <p><strong><a style="text-decoration:none;color:white !important" href="www.leguichet.mg">www.leguichet.mg</a> </strong></p>
+
+    <div id="section" style="width:100%;height:400px;background:white;">
+        <div id="message" style="position:relative;margin:auto;background: #d70506;width:100%;height:40px;top:5px;"> <p style=" text-align:center; color:white;font-size:30px;font-style:bold; padding-top:3px;"> SUCCES </p></div>
+        <div id="section-text" style="position:relative;padding-left:50px;padding-right:50px">
+
+
+            <div id="row1" style="padding-top:30px;">
+                Bonjour {{$user->name}},
+                <p> Votre événement a été ajouté avec <strong style="color:red;">SUCCES</strong>. Nous vous remercions de votre confiance.</p>
+
+
+            </div></br>
+
+            <div id="row2" style="padding-top:10px; width:500px;height:auto;">
+
+               <img src="{{url('/public/img/'.$event->image)}}"/>
+
+
+            </div></br>
+
+            <div id="row3" style="padding-top:170px;">
+
+                Pour consulter votre événement <strong style="font-style:italic; color:red;"><a href="https://leguichet.mg/event/show/'.$event->id" style="text-decoration:none;color:#d70506;">cliquez ici</a></strong>
             </div>
+
         </div>
+
+
     </div>
+
+    <div id="footer" style="width:100%;height:100px;background:#d70506;top:500px;">
+
+        <div id="textfooter" style="position:relative; width:400px; margin:auto; color:white; font-size:14px; padding-top:40px;text-align: center;">Pour toutes questions, consultez <a style="text-decoration:none;color:white;" href="https://leguichet.mg/faq">faq</a></div>
+    </div>
+
 </div>
 </body>
