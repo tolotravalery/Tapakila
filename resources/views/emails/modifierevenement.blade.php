4d6627e446d8d261edb394ae4a301c0d3145803e
Andrysitraka <andrewsitraka@gmail.com>
2018-02-13 17:02:55 +0300

    page evenement email,reinitialisation mdp et mise à jour événément

--- resources/views/emails/modifierevenement.blade.php
+++ resources/views/emails/modifierevenement.blade.php
@@ -32,7 +32,7 @@
 <div id="cp">
     <div id="header" style="width:100%;height:100px;background:#d70506;color:white;">
         <img src="{{url('/')}}/public/img/logo.png" style="padding-left: -5px;">
-        <div id="textheader" style="position:relative; width:400px; margin:auto;  font-size:18px; padding-top:40px;text-align: center; text-decoration:none;color:white;"><a style="text-decoration:none;color:white;" href="https://leguichet.mg/faq">www.leguichet.mg</a></div>
+        <div id="textheader" style="position:relative; width:400px; margin:auto;  font-size:18px; padding-top:40px;text-align: center; text-decoration:none;color:white;"><a style="text-decoration:underline;color:white;" href="https://leguichet.mg/faq">www.leguichet.mg</a></div>
     </div>
 
     <div id="section" style="width:100%;height:400px;background:white;">
@@ -56,7 +56,7 @@
 
             <div id="row3" style="padding-top:170px;">
 
-                Pour consulter votre événement <strong style="font-style:italic; color:red;"><a href="https://leguichet.mg/event/show/'.$event->id" style="text-decoration:none;color:#d70506;">cliquez ici</a></strong>
+                Pour consulter votre événement <strong style="font-style:italic; color:red;"><a href="https://leguichet.mg/event/show/'.$event->id" style="text-decoration:underline;color:#d70506;">cliquez ici</a></strong>
             </div>
 
         </div>
@@ -66,7 +66,7 @@
 
     <div id="footer" style="width:100%;height:100px;background:#d70506;top:500px;">
 
-        <div id="textfooter" style="position:relative; width:400px; margin:auto; color:white; font-size:14px; padding-top:40px;text-align: center;">Pour toutes questions, consultez <a style="text-decoration:none;color:white;" href="https://leguichet.mg/faq">faq</a></div>
+        <div id="textfooter" style="position:relative; width:400px; margin:auto; color:white; font-size:14px; padding-top:40px;text-align: center;">Pour toutes questions, consultez <a style="text-decoration:underline;color:white;" href="https://leguichet.mg/faq">faq</a></div>
     </div>
 
 </div>
