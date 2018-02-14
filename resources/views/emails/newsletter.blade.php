5c890e43229b56729ef3c1c48bd83423d25bfaa1
tolotravalery <valeryharisoanitolotra@gmail.com>
2018-02-14 11:00:40 +0100

    integration email newsletter

--- resources/views/emails/newsletter.blade.php
+++ resources/views/emails/newsletter.blade.php
@@ -1,3 +1,4 @@
+{{--
 <style>
     @media only screen and (max-width: 600px) {
         .inner-body {
@@ -78,4 +79,40 @@
         </td>
     </tr>
 </table>
+</body>--}}
+        <!DOCTYPE html>
+<html lang="fr">
+<head>
+</head>
+<body>
+<body width="800px">
+<div class="cp" style="position:relative;width:800px;margin:auto;height:auto;">
+    <div class="header" style="position:relative;width:100%;height:60px;margin:auto;background:#d70506">
+        <div class="logo" style="position:absolute;width:15%;height:80px;top:5px;left:10px;">
+            <img src="{{url('/')}}/public/img/logo.png">
+        </div>
+        <div class="titre" style="position:absolute;width:80%;height:80px;float:right; left:20%;top:2px;">
+            <a href="{{url('/')}}" style="position:absolute;text-decoration:underline;color:white; left:150px;padding-top:20px;" ><b>www.leguichet.mg</b></a>
+
+        </div>
+    </div>
+    <div class="section" style="position:relative;width:100%;height:400px;margin:auto;margin:50px;">
+        <p>Bonjour {{$user->name}},</p>
+        <p>Ne ratez pas les événements importants sur leguichet.mg. Venez visiter le site web</br>
+            <a href="{{url('/')}}" style="text-decoration:underline;color:#d70506">www.leguichet.mg</a></p>
+        <div class="image" style="position:relative;width:500px;height:220px;border:solid grey 1px;"><img style="width:500px;height:220px;" src="{{url('/')}}/public/img/{{$event->image}}"></div>
+        <div style="width:500px;">
+            <p>Nom de l’évenement :{{$event->title}}</p>
+            <p>Description :{{$event->additional_note}}</p>
+            <p>Lieu : {{$event->localisation_nom}} {{$event->localisation_adresse}}</p>
+            <p>Date : le {{$event->date_debut_envent->format('d M Y H:i:s')}} à {{$event->date_fin_event->format('d M Y H:i:s')}}</p>
+            <p>Vous recevez cet email car vous êtes abonné au newsletter de <a href="{{url('/')}}" style="text-decoration:underline;color:#d70506">www.leguichet.mg</a></p>
+        </div>
+    </div>
+    <div class="footer" style="position:relative;width:100%;height:60px;margin:auto;background:#d70506;">
+        <a href="{{url('/')}}" style="position:absolute;width:auto;text-decoration:underline;color:white;padding-top:18px;text-align:center;left:300px;" ><b>www.leguichet.mg</b></a>
+    </div>
+
+</div>
 </body>
+</html>
