0480be2c983a9534027205572772601140b66522
tolotravalery <valeryharisoanitolotra@gmail.com>
2018-02-14 12:08:20 +0100

    integration template email

--- resources/views/emails/forgotpwd.blade.php
+++ resources/views/emails/forgotpwd.blade.php
@@ -1,4 +1,4 @@
-<style>
+{{--<style>
     @media only screen and (max-width: 600px) {
         .inner-body {
             width: 100% !important;
@@ -59,4 +59,38 @@
         </div>
     </div>
 </div>
+</body>--}}
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+</head>
+<body width="800px">
+<div class="cp" style="position:relative;width:800px;margin:auto;height:auto;">
+    <div class="header" style="position:relative;width:100%;height:60px;margin:auto;background:#d70506">
+        <div class="logo" style="position:absolute;width:15%;height:80px;top:5px;left:10px;">
+            <img src="{{url('/')}}/public/img/logo.png">
+        </div>
+        <div class="titre" style="position:absolute;width:80%;height:80px;float:right; left:20%;top:2px;">
+            <a href="{{url('/')}}" style="position:absolute;text-decoration:underline;color:white; left:150px;padding-top:20px;" ><b>www.leguichet.mg</b></a>
+
+        </div>
+    </div>
+
+    <div class="header" style="position:relative;width:100%;height:50px;margin:auto; ;top:10px;">
+        <div class="message" style="position:absolute;left:300px;font-style:bold;color:black;width:250px;top:15px;text-decoration:underline;font-size:25px">Mot de passe oublié</div>
+    </div>
+    <div class="section" style="position:relative;width:100%;height:auto;margin:auto;margin:50px;">
+
+        <p>Vous avez demandé à changer le mot de passe de votre compte leguichet ? </br>Pour créer un nouveau mot de passe cliquez sur le lien ci-dessous :<br/></p>
+        <div class="bouton" style="position:absolute;width:300px;height:50px;left:200px;background:black;"><a href="{{$actionUrl}}" style="position:absolute;text-decoration:underline;color:white; left:35px;padding-top:13px;" ><b>Réinitialiser votre mot de passe</b></a></div>
+
+    </div>
+
+    <div class="footer" style="position:relative;width:100%;height:60px;margin:auto;background:#d70506;top:20px;">
+        <a href="{{url('/')}}" style="position:absolute;width:auto;text-decoration:underline;color:white;padding-top:18px;text-align:center;left:300px;" ><b>www.leguichet.mg</b></a>
+    </div>
+
+</div>
 </body>
+</html>
--- resources/views/emails/modifierevenement.blade.php
+++ resources/views/emails/modifierevenement.blade.php
@@ -1,5 +1,5 @@
 
-<style>
+{{--<style>
     @media only screen and (max-width: 600px) {
         .inner-body {
             width: 100% !important;
@@ -71,4 +71,39 @@ border-radius: 3px;
         </td>
     </tr>
 </table>
+</body>--}}
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+</head>
+<body width="800px">
+<div class="cp" style="position:relative;width:800px;margin:auto;height:auto;">
+    <div class="header" style="position:relative;width:100%;height:60px;margin:auto;background:#d70506">
+        <div class="logo" style="position:absolute;width:15%;height:80px;top:5px;left:10px;">
+            <img src="{{url('/')}}/public/img/logo.png">
+        </div>
+        <div class="titre" style="position:absolute;width:80%;height:80px;float:right; left:20%;top:2px;">
+            <a href="{{url('/')}}" style="position:absolute;text-decoration:underline;color:white; left:150px;padding-top:20px;" ><b>www.leguichet.mg</b></a>
+
+        </div>
+    </div>
+
+    <div class="header" style="position:relative;width:100%;height:50px;margin:auto;background:#d70506 ;top:10px;">
+        <div class="message" style="position:absolute;left:350px;font-style:bold;color:white;width:100px;top:15px;">SUCCESS</div>
+    </div>
+    <div class="section" style="position:relative;width:100%;height:auto;margin:auto;margin:50px;">
+        <p>Bonjour {{$user->name}},</p>
+        <p>La mise à jour de votre événement est réussie.</br>
+        <div class="image" style="position:relative;width:500px;height:220px;border:solid grey 1px;"><img style="width:500px;height:220px;" src="{{url('/')}}/public/img/{{$event->image}}"></div>
+        <p>Pour consulter votre événement <a href="{{url('event/show',[$event->id])}}" style="color:#d70506;text-decoration:underline">cliquez ici</p>
+    </div>
+
+    <div class="footer" style="position:relative;width:100%;height:60px;margin:auto;background:#d70506;">
+        <a href="{{url('/')}}" style="position:absolute;width:auto;text-decoration:underline;color:white;padding-top:18px;text-align:center;left:300px;" ><b>www.leguichet.mg</b></a>
+    </div>
+
+</div>
 </body>
+</html>
