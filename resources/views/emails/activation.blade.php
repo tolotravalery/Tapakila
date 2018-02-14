41b3b425923401df960f29ba076871d70dbb81bb
tolotravalery <valeryharisoanitolotra@gmail.com>
2018-02-14 15:56:44 +0100

    fix activation email front

--- resources/views/emails/activation.blade.php
+++ resources/views/emails/activation.blade.php
@@ -10,7 +10,16 @@
     }
 
 </style>
-<body style="background-color:#eeeeee;">
+<body style="background-color:white;">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-offset-3 col-lg-6">
+            <div style="height: 0px; color: white;text-align: center;background-color: #d70506;font-size: 16px;padding: 2px;font-family:sans-serif;">
+                <p><a style="text-decoration:none;color:white !important;" href="www.leguichet.mg">www.leguichet.mg</a> </p>
+            </div>
+        </div>
+    </div>
+</div>
 <table class="wrapper" width="100%" cellpadding="0" cellspacing="0">
     <tr>
         <td style="background-color: #d70506;padding: 2px">
@@ -29,16 +38,14 @@
 
 <table class="wrapper" width="100%" cellpadding="0" cellspacing="0" style="margin-top:-20px">
     <tr>
-        <td align="center">
-            <h2>
-                <br/><b style=" color:#333;margin-top: 35px; margin-bottom: 10px;font-size: 30px;word-wrap: break-word;font-weight: 700;font-family:Lucida Console;">Bonjour,</b>
-            </h2>
-            <p style="font-size: 14px;color:#333;">Nous vous remerçions de votre inscription et somme heureux de vous compter parmi nos utilisateurs.</p>
-            <p style="font-size: 14px;color:#333;">Pensez a conserver l'adresse mail que vous avez saisie afin de trouver aisément votre mot de passe. </p>
-			<p style="font-size: 14px;color:#333;"><b>Cliquez sur "Activer" pour confirmer votre inscription</b></p><br>
-			<div style="  text-align: center;position: relative;min-height: 1px;margin-left: auto; margin-right: auto;width: 33.33333333%;">
-				<a href="{{$actionUrl}}" style=" display:block;text-decoration: none;background-color: black; color: white;padding: 10px 16px;font-size: 18px; line-height: 1.3333333;border-radius: 6px; "> {{$actionText}} </a>
-			</div>
+        <td align="left">
+
+            <p style="font-size: 14px;color:#333;padding-left:50px;padding-top:20px;">Bonjour.</p>
+            <p style="font-size: 14px;color:#333;padding-left:50px;padding-top:0px;"><strong>Vous y êtes presque !</strong> Cliquez sur le lien ci-dessous pour activer votre compte.</p>
+
+            <div style="  text-align: center;position: relative;min-height: 1px;margin-left: auto; margin-right: auto;width: 20%;">
+                <a href="{{$actionUrl}}" style=" display:block;text-decoration: none;background-color: black; color: white;padding: 10px 16px;font-size: 18px; line-height: 1.3333333;border-radius: 6px; "> Activer </a>
+            </div>
 
         </td>
     </tr>
@@ -53,7 +60,7 @@
     <div class="row">
         <div class="col-lg-offset-3 col-lg-6">
             <div style=" color: white;text-align: center;background-color: #d70506;font-size: 16px;padding: 1px;font-family:sans-serif;">
-                <p><strong><a style="text-decoration:none;color:white !important" href="www.leguichet.mg">www.leguichet.mg</a> </strong></p>
+                <p>Pour toutes questions, consultez <a style="text-decoration:underline;color:white !important" href="www.leguichet.mg">faq</a> </p>
             </div>
         </div>
     </div>
