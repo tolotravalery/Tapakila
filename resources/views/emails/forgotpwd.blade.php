9b8209035c42d9966daf66170709c7ce8ddca478
tolotravalery <valeryharisoanitolotra@gmail.com>
2018-02-15 14:26:10 +0100

    Integration template email:ajout,modifevent,forgotpwd,newsletter,ticket

--- resources/views/emails/forgotpwd.blade.php
+++ resources/views/emails/forgotpwd.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{--<!DOCTYPE html>
 <html lang="fr">
 <head>
 </head>
@@ -30,5 +30,86 @@
 
 </div>
 </body>
-</html>
+</html>--}}
 
+<style>
+    @media only screen and (max-width: 600px) {
+        .inner-body {
+            width: 100% !important;
+        }
+
+        .footer {
+            width: 100% !important;
+        }
+    }
+
+</style>
+<body style="background-color:white;">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-offset-3 col-lg-6">
+            <div style="height: 0px; color: white;text-align: center;background-color: #d70506;font-size: 16px;padding: 2px;font-family:sans-serif;">
+                <p><a style="text-decoration:none;color:white !important;" href="{{url('/')}}">www.leguichet.mg</a> </p>
+            </div>
+        </div>
+    </div>
+</div>
+<table class="wrapper" width="100%" cellpadding="0" cellspacing="0">
+    <tr>
+        <td style="background-color: #d70506;padding: 2px">
+            <img src="{{url('/')}}/public/img/logo.png" style="padding-left: -5px;">
+        </td>
+    </tr>
+</table>
+<br/>
+<div class="container">
+    </br>
+    <div class="row">
+        <div class="col-lg-offset-3 col-lg-6">
+            <div style="; color: white;text-align: center;background-color: #d70506;font-size: 16px;padding: 1px;font-family:sans-serif;">
+                <p><b style="text-decoration:none;color:white !important;">Mot de passe oublié</b> </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<table class="wrapper" width="100%" style="margin-top:-20px;margin-left:18px; ">
+    <tr>
+        <td>
+            <h3 style="font-size: 30px; font-family:sans-serif;color:#333;"><b></b></h3>
+            <p style="font-family:sans-serif;font-size: 18px;color: #d70506;padding-top: -34px;margin: 0 0 10px;"></p>
+        </td>
+    </tr>
+</table>
+
+<table class="wrapper" width="100%" cellpadding="0" cellspacing="0" style="margin-top:-20px">
+    <tr>
+        <td align="center">
+
+            <p style="font-size: 14px;color:#333;padding-left:50px;padding-top:20px;">Bonjour.</p>
+            <p style="font-size: 14px;color:#333;padding-left:50px;padding-top:0px;">Vous avez demandé à changer le mot de passe de votre compte leguichet ? Pour créer un nouveau mot de passe cliquez sur le lien ci-dessous :</p>
+
+            <div style="  text-align: center;position: relative;min-height: 1px;margin-left: auto; margin-right: auto;width: 30%;">
+                <a href="{{$actionUrl}}" style=" display:block;text-decoration: none;background-color: black; color: white;padding: 10px 16px;font-size: 18px; line-height: 1.3333333;border-radius: 6px; "> Réinitialiser votre mot de passe </a>
+            </div>
+
+        </td>
+    </tr>
+</table>
+<br/>
+<div class="container">
+    <div class="row ">
+
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-offset-3 col-lg-6">
+            <div style=" color: white;text-align: center;background-color: #d70506;font-size: 16px;padding: 1px;font-family:sans-serif;">
+                <p>Pour toutes questions, consultez <a style="text-decoration:underline;color:white !important" href="{{url('/')}}/faq">faq</a> </p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
--- resources/views/emails/modifierevenement.blade.php
+++ resources/views/emails/modifierevenement.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{--<!DOCTYPE html>
 <html lang="fr">
 <head>
 </head>
@@ -30,5 +30,97 @@
 
 </div>
 </body>
-</html>
+</html>--}}
 
+<style>
+    @media only screen and (max-width: 600px) {
+        .inner-body {
+            width: 100% !important;
+        }
+
+        .footer {
+            width: 100% !important;
+        }
+    }
+
+</style>
+<body style="background-color:white;">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-offset-3 col-lg-6">
+            <div style="height: 0px; color: white;text-align: center;background-color: #d70506;font-size: 16px;padding: 2px;font-family:sans-serif;">
+                <p><a style="text-decoration:none;color:white !important;" href="{{url('/')}}">www.leguichet.mg</a> </p>
+            </div>
+        </div>
+    </div>
+</div>
+<table class="wrapper" width="100%" cellpadding="0" cellspacing="0">
+    <tr>
+        <td style="background-color: #d70506;padding: 2px">
+            <img src="{{url('/')}}/public/img/logo.png" style="padding-left: -5px;">
+        </td>
+    </tr>
+</table>
+<br/>
+<div class="container">
+    </br>
+    <div class="row">
+        <div class="col-lg-offset-3 col-lg-6">
+            <div style="; color: white;text-align: center;background-color: #d70506;font-size: 16px;padding: 1px;font-family:sans-serif;">
+                <p><b style="text-decoration:none;color:white !important;">SUCCES</b> </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<table class="wrapper" width="100%" style="margin-top:-20px;margin-left:18px; ">
+    <tr>
+        <td>
+            <h3 style="font-size: 30px; font-family:sans-serif;color:#333;"><b></b></h3>
+            <p style="font-family:sans-serif;font-size: 18px;color: #d70506;padding-top: -34px;margin: 0 0 10px;"></p>
+        </td>
+    </tr>
+</table>
+
+<table class="wrapper" width="100%" cellpadding="0" cellspacing="0" style="margin-top:-20px">
+    <tr>
+        <td align="center">
+
+            <p style="font-size: 14px;color:#333;padding-left:20px;padding-top:20px;">Bonjour {{$user->name}}.</p>
+            <p style="font-size: 14px;color:#333;padding-left:20px;padding-top:0px;width:auto;">La mise à jour de votre événement est réussie.</p>
+
+            <p>
+                <div class="container">
+                    </br>
+                    <div class="row">
+                        <div class="col-lg-offset-3 col-lg-6">
+                            <div style="; color: white;text-align: center;font-size: 16px;padding: 0px;font-family:sans-serif;">
+                                <div class="image" style="position:relative;width:auto;height:auto;"><img style="width:auto;height:auto;" src="{{url('/')}}/public/img/{{$event->image}}"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </p>
+            <p style="font-size: 14px;color:#333;padding-left:20px;padding-top:0px;width:auto;">Pour consulter votre événement  <a style="color:#d70506" href="{{url('event/show',[$event->id])}}"> cliquez ici</a></p>
+
+        </td>
+    </tr>
+</table>
+<br/>
+<div class="container">
+    <div class="row ">
+
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-offset-3 col-lg-6">
+            <div style=" color: white;text-align: center;background-color: #d70506;font-size: 16px;padding: 1px;font-family:sans-serif;">
+                <p>Pour toutes questions, consultez <a style="text-decoration:underline;color:white !important" href="{{url('/')}}/faq">faq</a> </p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
